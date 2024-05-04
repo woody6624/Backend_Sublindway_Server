d7a03b9bf13ef80a6d8642b931e4d755c6ccd8b6
woody6624 <woody6624@gmail.com>
2024-05-04 23:28:15 +0900

    swagger 명세서 가독성 증진

--- src/main/java/SublindWay_server/config/CorsConfig.java
+++ src/main/java/SublindWay_server/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://*.localhost:3000")  // localhost:3000에서 시작하는 모든 서브도메인 허용
+                .allowedOriginPatterns("http://localhost:3000")  // localhost:3000에서 시작하는 모든 서브도메인 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600);
