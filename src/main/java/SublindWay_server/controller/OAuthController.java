c1498ae5a99e170970fea9c6494e98d3b416cc4f
woody6624 <woody6624@gmail.com>
2024-05-16 10:56:03 +0900

    카카오 로그아웃 추가

--- src/main/java/SublindWay_server/controller/OAuthController.java
+++ src/main/java/SublindWay_server/controller/OAuthController.java
@@ -14,6 +14,8 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -27,11 +29,13 @@ public class OAuthController {
     UserRepository userRepository;
     @ResponseBody
     @GetMapping("/kakao")
-    @Operation(summary = "웹에서 인가 코드 전달", description = "해당 인가 코드로 로그인 구현,유저의 id값 리턴")
-    public RedirectView kakaoLoginWeb(@RequestParam String code) {
-        String gugucaca = oAuthService.getKakaoAccessToken(code);
-        System.out.println(gugucaca);
-        UserDTO user = oAuthService.createKakaoUser(gugucaca);
+    public RedirectView kakaoLoginWeb(@RequestParam String code, HttpServletRequest request) {
+        String accessToken = oAuthService.getKakaoAccessToken(code);
+        UserDTO user = oAuthService.createKakaoUser(accessToken);
+
+        // 세션에 사용자 정보 저장
+        HttpSession session = request.getSession();
+        session.setAttribute("user", user);
 
         String userNameEncoded = "";
         try {
@@ -41,13 +45,13 @@ public class OAuthController {
         }
 
         String redirectUrl = "http://localhost:3000/locationMap?kakaoId=" + user.getKakaoId() + "&userName=" + userNameEncoded;
-
         return new RedirectView(redirectUrl);
     }
 
     @GetMapping("/logout")
     public String logout(HttpServletRequest request) {
         oAuthService.kakaoLogout();
+
         // 세션 무효화
         HttpSession session = request.getSession(false);
         if (session != null) {
@@ -56,10 +60,19 @@ public class OAuthController {
 
         return "로그아웃 성공";
     }
+
+    @GetMapping("/check-login")
+    public Map<String, Boolean> checkLogin(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        boolean isLoggedIn = session != null && session.getAttribute("user") != null;
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("isLoggedIn", isLoggedIn);
+        return response;
+    }
+
     @GetMapping(value="/get-access-token")
-    @Operation(summary = "유저 id로 엑세스토큰 get", description = "로그아웃 시 필요")
-    public String getAccessToken(String kakaoId){
-        Optional<UserEntity> userEntity =userRepository.findById(kakaoId);
+    public String getAccessToken(String kakaoId) {
+        Optional<UserEntity> userEntity = userRepository.findById(kakaoId);
         return userEntity.get().getAccessToken();
     }
 
