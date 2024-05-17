11287b40d3438da0731d8315b0f49b89564a447d
woody6624 <woody6624@gmail.com>
2024-05-17 13:59:26 +0900

    카카오 로그아웃 추가

--- src/main/java/SublindWay_server/utility/ApiCallScheduler.java
+++ src/main/java/SublindWay_server/utility/ApiCallScheduler.java
@@ -16,7 +16,7 @@ public class ApiCallScheduler {
         this.getAllSubwayTrainDataService = getAllSubwayTrainDataService;
     }
 
-    @Scheduled(fixedRate = 120000) // 120초마다 실행
+    @Scheduled(fixedRate = 60000) // 120초마다 실행
     public void myScheduledFunction() {
         try {
             getAllSubwayTrainDataService.getAllTrainData();
