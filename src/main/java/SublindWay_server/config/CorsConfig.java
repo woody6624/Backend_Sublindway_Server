6eb25f1df99f025a4818dc8bebf98b5db043f6cc
woody6624 <woody6624@gmail.com>
2024-05-20 20:05:25 +0900

    카카오 로그아웃 추가

--- src/main/java/SublindWay_server/config/CorsConfig.java
+++ src/main/java/SublindWay_server/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://13.209.7.161:80")  // http://13.209.7.161:80에서 시작하는 모든 서브도메인 허용
+                .allowedOriginPatterns("http://13.209.7.161")  // http://13.209.7.161에서 오는 모든 요청 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600);
