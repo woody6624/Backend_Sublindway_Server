9edb31ae129939b8ab8072484d96eb0f6bebb3c4
woody6624 <woody6624@gmail.com>
2024-05-20 12:54:10 +0900

    아오

--- src/main/java/SublindWay_server/service/SubwayUpDownCheckService.java
+++ src/main/java/SublindWay_server/service/SubwayUpDownCheckService.java
@@ -37,14 +37,14 @@ public class SubwayUpDownCheckService {
         switch (line) {
             case "01호선":
             case "04호선":
-                return (start < end) ? "하행" : "상행";
+                return (start < end) ? "상행" : "하행";
             case "03호선":
             case "05호선":
             case "06호선":
             case "07호선":
             case "08호선":
             case "09호선":
-                return (start < end) ? "상행" : "하행";
+                return (start < end) ? "하행" : "상행";
             default:
                 return "알 수 없는 호선입니다.";
         }
