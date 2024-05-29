38d3eeda158c902771f0961a162c3a8fbaf0ea32
woody6624 <woody6624@gmail.com>
2024-05-29 15:01:50 +0900

    리팩토링

--- src/main/java/SublindWay_server/service/SubwayUpDownCheckService.java
+++ src/main/java/SublindWay_server/service/SubwayUpDownCheckService.java
@@ -41,7 +41,7 @@ public class SubwayUpDownCheckService {
             case "03호선":
             case "05호선":
             case "06호선":
-                return (start < end) ? "상행" : "하행";
+                return (start < end) ? "하행" : "상행";
             case "07호선":
             case "08호선":
             case "09호선":
